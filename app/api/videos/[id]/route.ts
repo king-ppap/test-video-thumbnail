b902a82baffde76fadccf325c3ff4ed4c5bfa8e2
King-Ponpawit <ponpawit.p@thelivingos.com>
2023-11-29 22:49:32 +0700

    Add api video

---
+++ app/api/videos/[id]/route.ts
@@ -0,0 +1,14 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    try {
+        const { rows } = await sql`SELECT * FROM videos WHERE id = ${params.id};`;
+        if (!rows.length) {
+            return NextResponse.json({ error: "Not found" }, { status: 404 });
+        }
+        return NextResponse.json(rows[0], { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 });
+    }
+}
